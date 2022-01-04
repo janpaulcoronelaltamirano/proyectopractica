cacfccd5c335378c066320fc0e0eedfc9924d34f
janpaul <theshaddixx12@gmail.com>
2022-01-04 08:02:27 -0500

    Primer commit de JAN

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Modificando la web de REACT by SHADDIXX
         </p>
         <a
           className="App-link"
